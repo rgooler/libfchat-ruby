089e0dabc17bc06d8593f6686f8f81486bc05d0e
Ryan Gooler <cheetahmorph@gmail.com>
2012-07-07 02:18:49 +0000

    Make libfchat identify itself

--- Gemfile
+++ Gemfile
@@ -1,11 +1,11 @@
 source "http://rubygems.org"
 
 gem 'multi_json','~> 1.3.6'
+gem 'em-http-request', '~> 1.0.2'
 
 group :test do
     gem 'rake'
     gem 'turn', '~> 0.9.5'
     gem 'minitest'
     gem 'facets'
-    gem 'em-http-request', '~> 1.0.2'
 end
--- lib/libfchat/fchat.rb
+++ lib/libfchat/fchat.rb
@@ -4,31 +4,53 @@ module Libfchat
   rescue LoadError
     #I don't actually NEED rubygems, unless on 1.8
   end
-  require 'net/http'
   require 'multi_json'
   require 'eventmachine'
   require 'em-http-request'
+  require 'libfchat/version'
   require 'libfchat/webapi'
   
   class Fchat
     attr_reader :ticket
+    attr_accessor :http
 
-    def login(server,account,password,timeout=30)
+    attr_reader :version
+    attr_reader :clientname
+
+    def initialize(clientname="libfchat-ruby by Jippen Faddoul ( http://github.com/jippen/libfchat-ruby )",version=Libfchat::VERSION)
+      @clientname = clientname
+      @version = version
+    end
+
+    def login(server,account,password,character,timeout=30)
       webapi = Libfchat::WebAPI.new
       @ticket = webapi.get_ticket(account,password)
 
       EventMachine.run {
-        http = EventMachine::HttpRequest.new(server).get :timeout => timeout
-        http.errback { puts "Could not connect to " + server }
-        http.callback { 
+        self.http = EventMachine::HttpRequest.new(server).get :timeout => timeout
+        self.http.errback { puts "Could not connect to " + server }
+        self.http.callback { 
           puts "Websocket connected"
+          self.IDN()
         }
 
-        http.stream { |msg|
+        self.http.stream { |msg|
           puts "Received: #{msg}"
         }
       }
     end
+
+    def IDN(account,character,ticket,cname=@clientname,cversion=@version,method="ticket")
+      # Initial identification with the server
+      json = {:account   => account,
+              :character => character,
+              :ticket    => ticket,
+              :cname     => cname,
+              :cversion  => cversion,
+              :method    => ticket}
+      self.http.send( "IDN " + MultiJson.dump(json) )
+    end
+
 
   end
 end
